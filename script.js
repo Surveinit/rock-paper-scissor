34b1352290e0e68449df92e263a4ea83230d6574
Surve <surve8729@gmail.com>
2024-06-30 22:51:13 +0530

    Added Human input logic

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
+let humanScore = 0;
+
+
 function getComputerChoice(random_num){
     if (random_num === 0) {
         console.log('Rock');
+        return 'Rock';
     }
     
     if (random_num === 1) {
         console.log('Paper');
+        return 'Paper';
     }
 
     if (random_num === 2) {
         console.log('Scissor');
+        return 'Scissor';
     }
     
 }
 
 getComputerChoice(Math.floor(Math.random() * 3))
+
+
+function getHumanChoice(){
+    let human_choice = prompt('Whats your choice, Human?')
+    console.log(human_choice)
+    return human_choice;
+}
+
+
+getHumanChoice()
